d9b04204c9fc891cb9a9ddf8466ecf18ab49c428
Clownsw <msmliexx1@gmail.com>
2022-11-18 21:56:45 +0800

    update `spring-boot version`
    Signed-off-by: Clownsw <msmliexx1@gmail.com>

--- src/main/java/cn/smilex/springbootvirtualtheaddemo/SpringbootVirtualTheadDemoApplication.java
+++ src/main/java/cn/smilex/springbootvirtualtheaddemo/SpringbootVirtualTheadDemoApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootVirtualtheadDemoApplication {
+public class SpringbootVirtualTheadDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootVirtualtheadDemoApplication.class, args);
+        SpringApplication.run(SpringbootVirtualTheadDemoApplication.class, args);
     }
-
 }
